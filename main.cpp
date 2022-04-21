423bd037113b5ec11f1c06e3f64aa98d7988835b
WangFiona <fionaw5th@gmail.com>
2022-04-21 14:28:53 -0700

    more work

--- main.cpp
+++ main.cpp
@@ -1,7 +1,307 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
+/*
+ * Author: Fiona Wang
+ * Date: April 18, 2022
+ * This program takes an input from the console or a file and builds a binary search tree
+ */
+
+//Class for the nodes used in the binary tree
+class BNode {
+  int data;
+  BNode* left;
+  BNode* right;
+
+public:
+  //Constructors
+  BNode(int newData){
+    data = newData;
+    left = NULL;
+    right = NULL;
+  }
+
+  //Set and get functions
+  void setLeft(BNode* newLeft){
+    left = newLeft;
+  }
+
+  void setRight(BNode* newRight){
+    right = newRight;
+  }
+
+  BNode* getLeft(){
+    return left;
+  }
+
+  BNode* getRight(){
+    return right;
+  }
+
+  void setData(int newData){
+    data = newData;
+  }
+
+  int getData(){
+    return data;
+  }
+};
+
+//Declaring the functions
+void addConsole(BNode* &tree);
+void addFile(BNode* &tree);
+void add(BNode* tree, int data);
+void printTree(BNode* tree, int count);
+int search(BNode* tree, int searchNum);
+void deleteOne(BNode* tree, BNode* previous, int deleteNum, int root);
+
 int main() {
+  //Initializing variables
+  BNode* tree = new BNode(-1);
+  char command[15];
+  char fileType[10];
+  bool running = true;
+
+  //Ask the user for a command
+  cout << "Enter a command (enter add, search, delete, print, or quit)" << endl;
+  while(running == true){
+    cin >> command;
+    
+    //Allow both upper and lower case
+    for(int i=0; i < strlen(command); i++) {
+      command[i] = toupper(command[i]);
+    }
+    
+    if(strcmp(command, "ADD") == false){
+      cout << "Enter a input type (console or file)" << endl;
+      cin >> fileType;
+      
+      //Allow both upper and lower case
+      for(int i=0; i < strlen(fileType); i++) {
+	fileType[i] = toupper(fileType[i]);
+      }
+
+      //Ask the user if they want to add from the console or a file
+      if(strcmp(fileType, "CONSOLE") == false){
+	addConsole(tree);
+      }
+      else if(strcmp(fileType, "FILE") == false){
+	addFile(tree);
+      }
+      else{
+	cout << "Not a valid file type" << endl;
+      }
+
+      cout << "Enter a command (enter add, search, delete, print, or quit)" << endl;
+    }
+    else if(strcmp(command, "SEARCH") == false){
+      cout << "What number do you want to find?" << endl;
+      int searchNum;
+      cin >> searchNum;
+      int found = search(tree, searchNum);
+      if(found == searchNum)
+	cout << searchNum << " exists in the tree!" << endl;
+      else
+	cout << searchNum << " does not exist in the tree!" << endl;
+      cout << "Enter a command (enter add, search, delete, print, or quit)" << endl;
+    }
+    else if(strcmp(command, "DELETE") == false){
+      cout << "What number do you want to delete?" << endl;
+      int deleteNum;
+      cin >> deleteNum;
+      int found = search(tree, deleteNum);
+      int root = tree->getData();
+      if(found == deleteNum)
+        deleteOne(tree, tree, deleteNum, root);
+      else
+        cout << deleteNum << " does not exist in the tree!" << endl;
+
+      cout << "Enter a command (enter add, search, delete, print, or quit)" << endl;
+    }
+    else if(strcmp(command, "PRINT") == false){
+      printTree(tree, 0);
+      cout << "Enter a command (enter add, search, delete, print, or quit)" << endl;
+    }
+    else if(strcmp(command, "QUIT") == false){
+      running = false;
+    }
+    else{
+      cout << "Please enter a valid command:" << endl;
+    }
+  }
+
   return 0;
+}
+
+//Function to take an input from a file
+void addFile(BNode* &tree){
+  char* fileName = new char[20];
+
+  //Ask the user to enter the file name
+  cout << "Enter the name of the file: (include the '.txt')" << endl;
+  cin >> fileName;
+  fstream file;
+  file.open(fileName);
+
+  //Extract numbers from the file
+  int input = 0;
+  while(file >> input){
+    add(tree, input);
+  }
+  file.close();
+}
+
+//Function to take an input from the console
+void addConsole(BNode* &tree){
+  bool entering = true;
+  char input[10];
+
+  //Ask the user for numbers or the word "done"
+  cout << "Enter a number (1-999) or enter \"done\"" << endl;
+
+  //Keep asking for input until the user enters "done"
+  while(entering){
+    cin >> input;
+    //Allow both upper and lower case
+    for(int i=0; i < strlen(input); i++) {
+      input[i] = toupper(input[i]);
+    }
+
+    //Check if the input is a number or "done"
+    bool isNum = true;
+    for(int i=0; i<strlen(input); i++){
+      if(!isdigit(input[i])){
+	isNum = false;
+      }
+    }
+
+    //If the user is done, leave the loop
+    if(strcmp(input, "DONE") == false){
+      entering = false;
+    }
+    //Keep asking for numbers
+    else if(!isNum){
+      cout << "Enter a number (1-999) or enter \"done\"" << endl;
+    }
+    else{
+      int num = atoi(input);
+      add(tree, num);
+      cout << "Enter a number (1-999) or enter \"done\"" << endl;
+    }
+  }
+}
+
+//Function to add into the tree
+void add(BNode* tree, int data){
+  //Check if you are adding at the root
+  if(tree->getData() == -1) {
+    tree->setData(data);
+    return;
+  }
+  else {
+    //Check if the new node should go to the right or left
+    //If to the left, check if it is at the base of the tree
+    if(data < tree->getData() && tree->getLeft()){
+      tree = tree->getLeft();
+      add(tree, data);
+    }
+    //Add the new node
+    else if(data < tree->getData() && !tree->getLeft()){
+      BNode* newNode = new BNode(data);
+      tree->setLeft(newNode);
+      return;
+    }
+
+    //If to the right, check if it is at the base of the tree
+    if(data > tree->getData() && tree->getRight()){
+      tree = tree->getRight();
+      add(tree, data);
+    }
+    //Add the new node
+    else if(data > tree->getData() && !tree->getRight()){
+      BNode* newNode = new BNode(data);
+      tree->setRight(newNode);
+      return;
+    }
+  }
+}
+
+//Function to print out the binary search tree
+void printTree(BNode* tree, int count) {
+  //Check if there is anything in the tree
+  if(tree->getData() == -1){
+    cout << "The tree is empty!" << endl;
+    return;
+  }
+
+  //Go through the right side of the tree
+  if(tree->getRight())
+    printTree(tree->getRight(), count+1);
+
+  //Print with right amount of tabs
+  for(int i=0; i<count; i++){
+    cout << "\t";
+  }
+  cout << tree->getData() << endl;
+
+  //Go through the left side of the tree
+  if(tree->getLeft())
+    printTree(tree->getLeft(), count+1);
+}
+
+//Function to check if a certain number exists in the tree
+int search(BNode* tree, int searchNum) {
+  if(!tree || tree->getData() == searchNum){
+    return tree->getData();
+  }
+
+  if(tree->getData() < searchNum){
+    if(!tree->getRight())
+      return tree->getData();
+    return search(tree->getRight(), searchNum);
+  }
+
+  else {
+    if(!tree->getLeft())
+      return tree->getData();
+    return search(tree->getLeft(), searchNum);
+  }
+}
+
+//Function to delete a specific node in the tree
+void deleteOne(BNode* tree, BNode* previous, int deleteNum, int root){
+  if(deleteNum == root){
+    
+  }
+  if(tree->getData() == deleteNum){
+    if(!tree->getRight() || !tree->getLeft()) {
+      if(tree->getData() < deleteNum){
+	tree->setData(tree->getRight()->getData());
+	tree->setRight(NULL);
+      }
+      else{
+	tree->setData(tree->getLeft()->getData());
+        tree->setLeft(NULL);
+      }
+      return;
+    }
+    else if(tree->getRight() && tree->getLeft()){
+      
+    }
+  }
+  if(tree->getData() < deleteNum){
+    deleteOne(tree->getRight(), tree, deleteNum, root);
+  }
+  else{
+    deleteOne(tree->getLeft(), tree, deleteNum, root);
+  }
+  /*else if(!tree->getRight() || !tree->getLeft()) {
+    if(tree->getData
+  }
+  else if(tree->getRight() && tree->getLeft()){
+    
+  }*/
 }
